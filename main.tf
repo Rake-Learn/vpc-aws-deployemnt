d04e9043ebbeb001c605b00f98a6719001133849
rake-learn <abburirakesh@gmail.com>
2024-11-11 20:08:28 -0500

    ficxed changes

--- main.tf
+++ main.tf
@@ -2,8 +2,8 @@ provider "aws" {
   region = "us-east-1"  # Change to your desired region
 }
 
-# tfsec:ignore:aws-ec2-require-vpc-flow-logs-for-all-vpcs
 # Create a VPC with a name tag
+# tfsec:ignore:aws-ec2-require-vpc-flow-logs-for-all-vpcs
 resource "aws_vpc" "main_vpc" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_support   = true
@@ -19,11 +19,11 @@ resource "aws_vpc" "main_vpc" {
 resource "aws_subnet" "public_subnet" {
   vpc_id                  = aws_vpc.main_vpc.id
   cidr_block              = "10.0.1.0/24"
-  availability_zone       = "us-east-1a"  # Change to the availability zone you want
+  availability_zone       = "us-east-1a"
   map_public_ip_on_launch = true  # Automatically associate public IPs with instances
 
   tags = {
-    Name = "public-subnet"  # Name for the subnet
+    Name = "public-subnet"
   }
 }
 
@@ -32,7 +32,7 @@ resource "aws_internet_gateway" "internet_gateway" {
   vpc_id = aws_vpc.main_vpc.id
 
   tags = {
-    Name = "my-internet-gateway"  # Name for the internet gateway
+    Name = "my-internet-gateway"
   }
 }
 
@@ -69,7 +69,7 @@ resource "aws_route_table" "public_route_table" {
   }
 
   tags = {
-    Name = "public-route-table"  # Name for the route table
+    Name = "public-route-table"
   }
 }
 
@@ -79,13 +79,13 @@ resource "aws_route_table_association" "public_route_table_association" {
   route_table_id = aws_route_table.public_route_table.id
 }
 
-# Enable VPC Flow Logs
 # tfsec:ignore:aws-ec2-require-vpc-flow-logs-for-all-vpcs
+# Enable VPC Flow Logs
 resource "aws_flow_log" "vpc_flow_log" {
   vpc_id         = aws_vpc.main_vpc.id
   traffic_type   = "ALL"
-  log_group_name = "vpc-flow-logs" 
-  log_stream_name = "vpc-flow-stream"
+  log_group_name = "vpc-flow-logs"  # Specify the CloudWatch Log Group
 
-  # Optional: Provide an IAM role if required iam_role_arn = "arn:aws:iam::YOUR_ACCOUNT_ID:role/YOUR_IAM_ROLE"
+  # Optional: Provide an IAM role if required
+  #iam_role_arn = "arn:aws:iam::YOUR_ACCOUNT_ID:role/YOUR_IAM_ROLE"
 }
