c1112512416e0062a29ee32d8ccab391ae6b3959
rake-learn <abburirakesh@gmail.com>
2024-11-15 11:16:20 -0500

    updated inbound rules for sg

--- main.tf
+++ main.tf
@@ -61,8 +61,8 @@ resource "aws_security_group" "allow_vpc_traffic" {
   ingress {
     from_port   = 22
     to_port     = 22
-    protocol    = "ssh"
-    cidr_blocks = ["0.0.0.0/0"]  # Allow HTTP traffic from anywhere
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # Allow ssh traffic from anywhere
   }
   ingress {
     from_port   = 0
@@ -71,18 +71,10 @@ resource "aws_security_group" "allow_vpc_traffic" {
     cidr_blocks = ["0.0.0.0/0"]  # Allow traffic from anywhere
   }
 
-  # Allow all TCP traffic within the same security group
-  ingress {
-    from_port       = 80
-    to_port         = 80
-    protocol        = "http"
-    self            = true  # Allows traffic within the same SG
-  }
-
   ingress {
     from_port   = 443
     to_port     = 443
-    protocol    = "https"
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]  # Allow HTTPS traffic from anywhere
   }
 
